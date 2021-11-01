d732bde13f1c548cdadd302b804c8cca30a0b070
Hao <1115626649@qq.com>
2021-11-01 10:52:45 +0800

    2021.11.1 环形链表

---
+++ hascycle/src/HasCycle.java
@@ -0,0 +1,27 @@
+public class HasCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
