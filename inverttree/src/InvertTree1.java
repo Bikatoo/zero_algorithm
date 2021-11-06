965c616b87cb29814a60d4cf0614a5fede05174d
Bikatoo <1115626649@qq.com>
2021-11-06 14:29:40 +0800

    2021.11.06

---
+++ inverttree/src/InvertTree1.java
@@ -0,0 +1,40 @@
+public class InvertTree1 {
+    public TreeNode invertTree(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+
+    private void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        dfs(node.left);
+        dfs(node.right);
+
+        //swap
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
