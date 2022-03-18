bf15e49b47079bd3391857c5046f2719e4cec365
Bikatoo <1115626649@qq.com>
2022-03-18 15:56:08 +0800

    2022.3.18 子集

--- subsets/src/Subsets.java
+++ subsets/src/Subsets.java
@@ -9,6 +9,7 @@ public class Subsets {
         res.add(new ArrayList<>());
 
         for (int n : nums) {
+            // 注意这里要记录下res此时的长度，如果直接在循环条件中写 res.size()的话循环无法结束，因为循环体中会增加元素。
             int resSize = res.size();
             for (int i = 0 ; i < resSize ; i++) {
                 List<Integer> list = res.get(i);
